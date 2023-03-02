e0bd65ca7725303dbb91f894a03910209110f9e9
yuutoyasue <uk27fbk@yahoo.co.jp>
2023-03-02 23:23:16 +0900

    更新

--- src/main/java/com/techacademy/CountryService.java
+++ src/main/java/com/techacademy/CountryService.java
@@ -1,9 +1,15 @@
 package com.techacademy;
 
 import java.util.List;
+import java.util.Optional;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
 
 @Service
 public class CountryService {
@@ -18,5 +24,27 @@ public class CountryService {
     public List<Country> getCountryList() {
         // リポジトリのfindAllメソッドを呼び出す
         return repository.findAll();
+    }
+
+    //1件を検索して返す
+    public Country getCountry(String code) {
+        //findByIdで検索
+        Optional<Country> option = repository.findById(code);
+        //取得できなかった場合はnullを返す
+        Country country = option.orElse(null);
+        return country;
+    }
+
+    //更新（追加）を行う
+    @Transactional
+    public void updateCountry(String code, String name, int population) {
+        Country country = new Country(code, name, population);
+        repository.save(country);
+    }
+
+    //削除を行う
+    @Transactional
+    public void deleteCountry(String code) {
+        repository.deleteById(code);
     }
 }
